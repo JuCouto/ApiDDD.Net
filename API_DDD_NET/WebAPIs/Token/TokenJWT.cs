478af5fe0e1e6b4a5add9908cd42aa5d9a65de14
Ju Couto <jnalmeida@ymail.com>
2022-12-05 18:26:20 -0300

    UserController e adicionando migration

--- API_DDD_NET/WebAPIs/Token/TokenJWT.cs
+++ API_DDD_NET/WebAPIs/Token/TokenJWT.cs
@@ -1,6 +1,18 @@
-﻿namespace WebAPIs.Token
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace WebAPIs.Token
 {
     public class TokenJWT
     {
+        private JwtSecurityToken token;
+
+        internal TokenJWT(JwtSecurityToken token)
+        {
+            this.token = token;
+        }
+
+        public DateTime ValidTo => token.ValidTo;
+
+        public string Value => new JwtSecurityTokenHandler().WriteToken(this.token);
     }
 }
