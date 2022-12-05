19e61c56cedc24c099d1cbeb52ca984896e0c8a1
Ju Couto <jnalmeida@ymail.com>
2022-12-05 15:25:56 -0300

    Configurações para Cors

--- API_DDD_NET/WebAPIs/Program.cs
+++ API_DDD_NET/WebAPIs/Program.cs
@@ -1,4 +1,17 @@
+using AutoMapper;
+using Domain.Interfaces;
+using Domain.Interfaces.Generics;
+using Domain.Interfaces.InterfaceServices;
+using Domain.Services;
+using Entities.Entities;
 using Infrastructure.Configuration;
+using Infrastructure.Repository.Generics;
+using Infrastructure.Repository.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using WebAPIs.Models;
+using WebAPIs.Token;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,9 +23,69 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
-// ConfigServices.
+// ConfigServices. --
 builder.Services.AddDbContext<ContextBase>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ContextBase>();
+
+
+// Configurações opcionais. --
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+// Configuração de interface e repositório(injeção de dependência). --
+builder.Services.AddSingleton(typeof(IGeneric<>), typeof(RepositoryGenerics<>));
+builder.Services.AddSingleton<IMessage, RepositoryMessage>();
+
+// Serviço Dominio. --
+builder.Services.AddSingleton<IServiceMessage, ServiceMessage>();
+
+// JWT. --
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(option =>
+    {
+        option.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            // Posso modificar o teste pelo nome da empresa por ex.
+            ValidIssuer = "Teste.Securiry.Bearer",
+            ValidAudience = "Teste.Securiry.Bearer",
+            // Criar um código da minha preferência.
+            IssuerSigningKey = JWTSecurityKey.Create("Secret_key-12345678")
+        };
+
+        option.Events = new JwtBearerEvents
+        {
+            OnAuthenticationFailed = context =>
+            {
+                Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+                return Task.CompletedTask;
+            },
+            OnTokenValidated = context =>
+            {
+                Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+                return Task.CompletedTask;
+            }
+        };
+    });
+
+// Configurações MAPPER. Models que serão automapedas .--
+var config = new AutoMapper.MapperConfiguration(cfg =>
+{
+    cfg.CreateMap<MessageViewModel, Message>();
+    cfg.CreateMap<Message, MessageViewModel>();
+});
+
+IMapper mapper = config.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+
 
 var app = builder.Build();
 
@@ -23,10 +96,27 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Forma 1, para utilizar o Cors. --
+//var urlDev = "https://dominiodocliente.com.br";
+//var urlDHML = "https://dominiodocliente2.com.br";
+//var urlPROD = "https://dominiodocliente3.com.br";
+
+//app.UseCors(b => b.WithOrigins(urlDEV, urlHML, urlPROD));
+
+
+// Forma 2, para utilizar o Cors. --
+var devClient = "http://localhost:4200";
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader().WithOrigins(devClient));
+
 app.UseHttpsRedirection();
 
+app.UseAuthentication(); // --
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseSwaggerUI(); // --
 
 app.Run();
