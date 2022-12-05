d94f75eef42f71f351fb317cf14a6491a033c557
Ju Couto <jnalmeida@ymail.com>
2022-12-05 15:05:43 -0300

    Projeto até configuração token e string conexão no program - aula 4

---
+++ API_DDD_NET/Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,45 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base (options)
+        {
+        }
+
+        // Passar aqui com DbSet para o entityFramework entender que eu quero criar no banco de dados
+        public DbSet<Message> Message { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        // Método para mapear a chave primária da tabela do identity.
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+            base.OnModelCreating(builder);
+        }
+
+
+        // Método para obter string de conexão.
+        public string ObterStringConexao()
+        {
+            return " Server=127.0.0.1;Port=5432;Database=Api_DDD_Net;User Id=postgres; Password=serratec2022";
+        }
+    }
+}
